77253d00a6e13b4cee78a3c585da8fa8a03cb990
Miras <91594617+laqube@users.noreply.github.com>
2023-02-05 22:05:11 +0600

    post scriptum commit
    Added comments and also uploaded test sets

--- main.py
+++ main.py
@@ -4,31 +4,32 @@ from bs4 import BeautifulSoup
 import urllib3
 import time
 
-dataset = []
-counter = 0
+dataset = []  #An empty dataset
+counter = 0   #A document counter
 
 http = urllib3.PoolManager()
 for p in range(1, 24):
     if p == 1:
-        url = 'https://www.olx.kz/d/kk/dom-i-sad/mebel/mebel-dlya-vannoy-komnaty'
+        url = 'https://www.olx.kz/d/kk/dom-i-sad/mebel/mebel-dlya-vannoy-komnaty'                 #First page of the category
     else:
-        url = 'https://www.olx.kz/d/kk/dom-i-sad/mebel/mebel-dlya-vannoy-komnaty/?page=' + str(p)
+        url = 'https://www.olx.kz/d/kk/dom-i-sad/mebel/mebel-dlya-vannoy-komnaty/?page=' + str(p) #Rest of the pages 
 
     response = http.request('GET', url)
     print("Status: " + str(response.status))
-    print("!____LESGOOOOOOOOOOOOOOOOOOOOOOOOOOOO____!")
-    print("!______________A_NEW_PAGE________________!")
-    print("PAGE NUMBER: " + str(p))
+    print("!____LESGOOOOOOOOOOOOOOOOOOOOOOOOOOOO____!")     #indication of successful connection
+    print("!______________A_NEW_PAGE________________!")             
+    print("PAGE NUMBER: " + str(p))                         #page counter
 
     soup = BeautifulSoup(response.data, "html.parser")
-    names = soup.find_all("a", {"class": "css-rc5s2u"})
+    names = soup.find_all("a", {"class": "css-rc5s2u"})     #all the links for certain products located on that page
 
     for name in names:
         purl = 'https://www.olx.kz/d/kk/obyavlenie' + name.get('href')
-        print('PRODUCT+1_________________________')
-        newresponse = http.request('GET', purl)
+        print('PRODUCT+1_________________________')         #indication of successfull connection to a product page
+        newresponse = http.request('GET', purl)             
 
         newsoup = BeautifulSoup(newresponse.data, "html.parser")
+                                                            #I used try-except block to ignore errors in case if one of the variables is null or of wrong datatype
         try:
             pName = newsoup.find("h1", {"class": "css-1soizd2 er34gjf0"}).get_text()
         except ValueError:
@@ -47,15 +48,15 @@ for p in range(1, 24):
             "Product_Name": pName,
             "Price": pPrice,
             "Tags": pTags
-        }
+        }                                                   #The structure of single document
         print(product)
         dataset.append(product)
         counter += 1
-    time.sleep(1)
+    time.sleep(1)                                           #Timer to avoid bot detection, actually in my case I believe it was useless
 
 print(dataset)  
-print("BOLDY__________________________")
-print("Product count: " + str(counter))
-print("ADIHAT_________________________")  
-with open("olx_dataset.json", "w") as outfile:
-    json.dump(dataset, outfile, ensure_ascii=False)
+print("BOLDY__________________________")                    #Indicator of successful completion of scrabbing 
+print("Product count: " + str(counter))                     #Document counter
+print("ADIHAT_________________________")                    
+with open("olx_dataset.json", "w") as outfile:              #Creating a json file
+    json.dump(dataset, outfile, ensure_ascii=False)         #ensure_ascii=False is used in order to decode the dataset right away
