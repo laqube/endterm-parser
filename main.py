93fb80222a77b28e58724c9b35304eb82c07587f
Miras <91594617+laqube@users.noreply.github.com>
2023-02-05 21:01:55 +0600

    final
    final commit with extracted json file

--- main.py
+++ main.py
@@ -4,64 +4,58 @@ from bs4 import BeautifulSoup
 import urllib3
 import time
 
-
-dataset =[]
+dataset = []
 counter = 0
 
+http = urllib3.PoolManager()
 for p in range(1, 24):
-    url=''
-    if(p==1):
+    if p == 1:
         url = 'https://www.olx.kz/d/kk/dom-i-sad/mebel/mebel-dlya-vannoy-komnaty'
     else:
         url = 'https://www.olx.kz/d/kk/dom-i-sad/mebel/mebel-dlya-vannoy-komnaty/?page=' + str(p)
-    # url="https://kolesa.kz/cars/"
 
-    dataset =[]
-    counter = 0
-    http = urllib3.PoolManager()
     response = http.request('GET', url)
     print("Status: " + str(response.status))
     print("!____LESGOOOOOOOOOOOOOOOOOOOOOOOOOOOO____!")
     print("!______________A_NEW_PAGE________________!")
-    print("PAGE NUMBER:" + str(p))
+    print("PAGE NUMBER: " + str(p))
 
-    soup = BeautifulSoup(response.data, features="html.parser")
-    names = soup.find_all("a", {"class":"css-rc5s2u"})
-    pCount = len(names)
-    pLinks = [] 
-    for i in range(0, pCount):
-        pLinks.append(names[i].get('href'))
-        purl = 'https://www.olx.kz/d/kk/obyavlenie' + names[i].get('href')
+    soup = BeautifulSoup(response.data, "html.parser")
+    names = soup.find_all("a", {"class": "css-rc5s2u"})
+
+    for name in names:
+        purl = 'https://www.olx.kz/d/kk/obyavlenie' + name.get('href')
         print('PRODUCT+1_________________________')
-        newhttp = urllib3.PoolManager()
-        newresponse = newhttp.request('GET', purl)
+        newresponse = http.request('GET', purl)
 
-        newsoup = BeautifulSoup(newresponse.data, features="html.parser")
-
-        pName = newsoup.find("h1", {"class": "css-1soizd2 er34gjf0"}).get_text()
-        pPrice = int("".join(newsoup.find("h3", {"class": "css-ddweki er34gjf0"}).get_text().split()[:-1]))
-        pTags = newsoup.find("ul", {"class":"css-sfcl1s"})
+        newsoup = BeautifulSoup(newresponse.data, "html.parser")
+        try:
+            pName = newsoup.find("h1", {"class": "css-1soizd2 er34gjf0"}).get_text()
+        except ValueError:
+            pName = "None"
+        try:
+            pPrice = int("".join(newsoup.find("h3", {"class": "css-ddweki er34gjf0"}).get_text().split()[:-1]))
+        except ValueError:
+            pPrice = 0
+        pTags = newsoup.find("ul", {"class": "css-sfcl1s"})
         wTag = pTags.find_all("p")
-        pTags = []
-        d = {}
-        # print(pTags)
-        # print(pTag)
-        for i in wTag:
-            # print(i.get_text())
-            pTags.append(i.get_text())
+        try: 
+            pTags = [i.get_text() for i in wTag]
+        except:
+            pTags = []
         product = {
-            'Product_Name':pName,
-            'Price':pPrice,
-            'Tags':pTags
+            "Product_Name": pName,
+            "Price": pPrice,
+            "Tags": pTags
         }
         print(product)
         dataset.append(product)
         counter += 1
     time.sleep(1)
+
 print(dataset)  
 print("BOLDY__________________________")
 print("Product count: " + str(counter))
 print("ADIHAT_________________________")  
-with open('olx_dataset.json', 'w') as outfile:
+with open("olx_dataset.json", "w") as outfile:
     json.dump(dataset, outfile, ensure_ascii=False)
-
